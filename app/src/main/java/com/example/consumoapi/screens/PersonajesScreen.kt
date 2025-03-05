e3488f9d8a9b08480edbabe5a7699172a4d118f4
Alextd7 <torresdonatealex@gmail.com>
2025-03-05 17:30:44 +0100

    añadido diseño

--- app/src/main/java/com/example/consumoapi/screens/PersonajesScreen.kt
+++ app/src/main/java/com/example/consumoapi/screens/PersonajesScreen.kt
@@ -1,101 +1,156 @@
 package com.example.consumoapi.screens
 
 import android.util.Log
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ExpandLess
+import androidx.compose.material.icons.filled.ExpandMore
 import androidx.compose.material3.Card
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.modifier.modifierLocalMapOf
 
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import coil.compose.AsyncImage
 import com.example.consumoapi.model.Personaje
+import kotlinx.coroutines.delay
+
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun PersonajesScreen(
-    viewModel: PersonajesViewModel = hiltViewModel()
-) {
+fun PersonajesScreen(viewModel: PersonajesViewModel = hiltViewModel()) {
     val state by viewModel.state.collectAsState()
-    Log.d("Cant. items", "${state.size}")
+    var isLoading by remember { mutableStateOf(true) }
+
+    LaunchedEffect(state) {
+        if (state.isEmpty()) {
+            delay(4000) //
+        }
+        isLoading = state.isEmpty()
+    }
 
     Scaffold(
+
         topBar = {
-            TopAppBar(
-                title = { Text(text = "Personajes Rick and Morty")}
-            )
+            TopAppBar(title = { Text(text = "Personajes Rick and Morty", color = Color.White) },
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primary))
         }
     ) { paddingValues ->
-        LazyColumn(
-            contentPadding = paddingValues
-        ) {
-            items(state) { personaje ->
-                PersonajeCard(personaje)
+        if (isLoading) {
+            Box(
+                modifier = Modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ) {
+                CircularProgressIndicator()
+            }
+        } else {
+            LazyColumn(contentPadding = paddingValues) {
+                items(state) { personaje ->
+                    PersonajeCard(personaje)
+                }
             }
         }
     }
 }
-
 
 
 @Composable
-fun PersonajeCard(personaje: Personaje,
-                  modifier: Modifier = Modifier
-)
-{
+fun PersonajeCard(personaje: Personaje, modifier: Modifier = Modifier) {
+    var expanded by remember { mutableStateOf(false) }
+
     Card(
         shape = MaterialTheme.shapes.medium,
-             modifier = modifier.fillMaxWidth()
-                .padding(horizontal = 8.dp, vertical = 4.dp))
-    {
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(horizontal = 16.dp, vertical = 8.dp)
 
+
+
+
+    ) {
         Column {
-            Row {
-                Surface (
-                    modifier.size(120.dp),
-                    color = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.2f))
-                {
-                    AsyncImage(
-
-                        model = personaje.image,
-                        contentDescription = personaje.name,
-                        contentScale = ContentScale.FillBounds
-                    )
-
-                  }
-
-                Column(modifier.padding(16.dp)
-                    .align(Alignment.CenterVertically)
-                    .weight(1f)
+            Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.End) {
+                Column(
+                    modifier = Modifier.padding(16.dp)
+                        .align(Alignment.CenterVertically)
+                        .weight(1f)
                 ) {
-                    Text(text= personaje.name,
+                    Text(
+                        text = personaje.name,
                         style = MaterialTheme.typography.titleLarge
-
                     )
 
+                    IconButton(
+                        onClick = { expanded = !expanded },
+                        modifier = Modifier.align(Alignment.Start)
+                    ) {
+                        Icon(
+                            imageVector = if (expanded) Icons.Filled.ExpandLess else Icons.Filled.ExpandMore,
+                            contentDescription = "Más información"
+                        )
+                    }
+
+                    if (expanded) {
+                        Row(modifier = Modifier.padding(12.dp)) {
+                            Column {
+                                Text(
+                                    text = "Última aparición",
+                                    style = MaterialTheme.typography.bodyMedium
+                                )
+                                Text(
+                                    text = personaje.location.name,
+                                    style = MaterialTheme.typography.bodyLarge
+                                )
+                            }
+                        }
+                    }
                 }
 
+                Surface(
 
+                    color = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.2f)
+                ) {
+                    AsyncImage(
+                        model = personaje.image,
+                        contentDescription = personaje.name,
+                        contentScale = ContentScale.FillBounds,
+
+                    )
+                }
             }
-
         }
-
     }
-
 }
+
